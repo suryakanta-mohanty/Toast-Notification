24e6a621ea5936968116ef2c2e4a376bf2428882
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2024-07-11 09:35:29 +0530

    adding icons to notifications with different color

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let toastBox = document.getElementById('toast-box');
 
-const successMsg = 'Successfully submitted';
-const errorMsg = 'Please fix the error!';
-const invalidMsg = 'Invalid input, check again';
+const successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+const errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error!';
+const invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
 
 
 
@@ -11,4 +11,12 @@ function showToast(msg){
   toast.classList.add('toast');
   toast.innerHTML = msg;
   toastBox.appendChild(toast);
+
+  if(msg.includes('error')){
+    toast.classList.add('error');
+  }
+  
+  if(msg.includes('Invalid')){
+    toast.classList.add('invalid');
+  }
 }
